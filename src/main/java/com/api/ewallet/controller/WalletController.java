3b84b11c0989d3cf155e56912bfc333dbba4f24e
IanPascua-PH <adrian.pascua@ibm.com>
2026-01-30 13:54:02 +0800

    chore: moved ewallet-schema.sql to docker directory, unused import cleanup

--- src/main/java/com/api/ewallet/controller/WalletController.java
+++ src/main/java/com/api/ewallet/controller/WalletController.java
@@ -6,7 +6,6 @@ import com.api.ewallet.service.WalletService;
 import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
