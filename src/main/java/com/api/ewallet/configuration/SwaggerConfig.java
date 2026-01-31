0ed82062d8b10f8ba25afe3cfd62c95a0827be02
IanPascua-PH <adrian.pascua@ibm.com>
2026-01-31 22:34:09 +0800

    refactor: Updated Swagger OpenAPI details

--- src/main/java/com/api/ewallet/configuration/SwaggerConfig.java
+++ src/main/java/com/api/ewallet/configuration/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package com.api.ewallet.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +14,16 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI walletControllerApi() {
         return new OpenAPI()
-                .info(new Info().title("E-Wallet App"));
+                .info(new Info()
+                        .title("E-Wallet App")
+                        .version("1.0.0")
+                        .description("Application for managing e-wallet operations like send money, get transaction details, check friend list and balance inquiry"))
+                .components(new Components()
+                        .addSecuritySchemes("userIdAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-User-Id")))
+                .addSecurityItem(new SecurityRequirement().addList("userIdAuth"));
     }
 }
